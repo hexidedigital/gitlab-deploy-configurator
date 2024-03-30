70ab383f3e3d9c2100ef3210b0bcb2a80113b62d
Oleksandr Moik <sashamoik2016@gmail.com>
2024-03-30 11:26:03 +0200

    Update base-edit-page.blade.php

--- resources/views/filament/pages/base-edit-page.blade.php
+++ resources/views/filament/pages/base-edit-page.blade.php
@@ -1,9 +1,7 @@
 <x-filament-panels::page>
     <x-filament-panels::form wire:submit="save">
         {{ $this->form }}
-
-        <x-filament-panels::form.actions
-            :actions="$this->getFormActions()"
-        />
     </x-filament-panels::form>
+
+    {{--<pre style="font-size: 0.7rem"><code>{{ json_encode($this->data, JSON_PRETTY_PRINT) }}</code></pre>--}}
 </x-filament-panels::page>
