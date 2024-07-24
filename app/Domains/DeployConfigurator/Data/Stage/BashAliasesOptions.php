dda42a3323aa15bf6d647d165b9818e8c6318834
Oleksandr Moik <sashamoik2016@gmail.com>
2024-07-24 10:58:24 +0300

    fix: set default values for bash aliases options

--- app/Domains/DeployConfigurator/Data/Stage/BashAliasesOptions.php
+++ app/Domains/DeployConfigurator/Data/Stage/BashAliasesOptions.php
@@ -5,22 +5,22 @@ namespace App\Domains\DeployConfigurator\Data\Stage;
 readonly class BashAliasesOptions
 {
     public function __construct(
-        public bool $insert,
-        public bool $artisanCompletion,
-        public bool $artisanAliases,
-        public bool $composerAlias,
-        public bool $folderAliases,
+        public bool $insert = false,
+        public bool $artisanCompletion = false,
+        public bool $artisanAliases = false,
+        public bool $composerAlias = false,
+        public bool $folderAliases = false,
     ) {
     }
 
     public static function makeFromArray(array $data): static
     {
         return new self(
-            insert: $data['insert'],
-            artisanCompletion: $data['artisanCompletion'],
-            artisanAliases: $data['artisanAliases'],
-            composerAlias: $data['composerAlias'],
-            folderAliases: $data['folderAliases'],
+            insert: data_get($data, 'insert', false),
+            artisanCompletion: data_get($data, 'artisanCompletion', false),
+            artisanAliases: data_get($data, 'artisanAliases', false),
+            composerAlias: data_get($data, 'composerAlias', false),
+            folderAliases: data_get($data, 'folderAliases', false),
         );
     }
 }
