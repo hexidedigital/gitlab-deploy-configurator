e28c514aca4e0b23e4760351319327e799553974
Oleksandr Moik <sashamoik2016@gmail.com>
2024-09-09 16:47:23 +0300

    fix: remove array keys for commit action payload

--- app/Domains/DeployConfigurator/Jobs/ConfigureRepositoryJob.php
+++ app/Domains/DeployConfigurator/Jobs/ConfigureRepositoryJob.php
@@ -379,7 +379,7 @@ class ConfigureRepositoryJob implements ShouldQueue
             $action['encoding'] = 'base64';
 
             return $action;
-        })->values()->filter();
+        })->filter()->values();
 
         $doNotUpdate = false; // todo
 
@@ -409,15 +409,6 @@ class ConfigureRepositoryJob implements ShouldQueue
 
         $message = str("Configure deployment 🚀")
             ->when($this->ciCdOptions->extra('use-prefix'), fn (Stringable $str) => $str->prepend("ci: "));
-
-        $this->logWriter->debug('Creating commit', [
-            "branch" => $this->currentStageInfo->name,
-            "start_branch" => $startBranch,
-            "commit_message" => $message,
-            "author_name" => "Deploy Configurator Bot",
-            "author_email" => "deploy-configurator-bot@hexide-digital.com",
-            "actions" => $actions->all(),
-        ]);
 
         $commit = $this->gitLabService->gitLabManager()->repositories()->createCommit($this->gitlabProject->id, [
             "branch" => $this->currentStageInfo->name,
